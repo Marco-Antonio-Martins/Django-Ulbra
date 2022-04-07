042d31af7b976e3dec6b316832ed55f181fc88e2
Marco Antônio <marcomartins06@rede.ulbra.br>
2022-04-07 08:05:58 -0300

    Alterações aula 07/04

--- app_noticias/admin.py
+++ app_noticias/admin.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
-from .models import Noticia
+from .models import Noticia, Pessoa, Tag
 
 # Register your models here.
 @admin.register(Noticia)
 class NoticiaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Pessoa)
+class PessoaAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
     pass
