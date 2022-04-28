42e6688c8fdb0f3ffee42649070b88409c8e238b
Marco Antonio Martins Porto Netto <marcomartins06@rede.ulbra.br>
2022-04-28 08:00:23 -0300

    Adicionando URLs e VIEWs

--- app_noticias/views.py
+++ app_noticias/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from app_noticias.models import Pessoa
 
-# Create your views here.
+def resumo_pessoas(request):
+    total = Pessoa.objects.count()
+    return HttpResponse('O total de pessoas é: {}'.format(total))
+
+def index(request):
+    return HttpResponse('Bem vindo a nossa página')
