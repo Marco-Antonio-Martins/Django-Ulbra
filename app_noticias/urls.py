a0f567cb4409022c1a8f64ae05dbe0a893613981
Marco Antonio Martins Porto Netto <marcomartins06@rede.ulbra.br>
2022-05-05 21:23:45 -0300

    Aula 05/05/2022

--- app_noticias/urls.py
+++ app_noticias/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from app_noticias.views import resumo_pessoas, index
+from app_noticias.views import resumo_pessoas, index, app_noticias_resumos_template, noticia_detalhes
 
 urlpatterns = [ #passar name=""
     path('', index, name="index"),
-    path('pessoas/', resumo_pessoas, name="resumo_pessoas"),    
+    path('pessoas/', resumo_pessoas, name="resumo_pessoas"), 
+    path('resumo/', app_noticias_resumos_template),   
+    path('noticias/<int:noticia_id>/', noticia_detalhes, name='detalhes'),
 ]
--- app_noticias/views.py
+++ app_noticias/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from app_noticias.models import Pessoa
+from app_noticias.models import Pessoa, Noticia
 
 def resumo_pessoas(request):
     total = Pessoa.objects.count()
@@ -8,3 +8,14 @@ def resumo_pessoas(request):
 
 def index(request):
     return HttpResponse('Bem vindo a nossa página')
+
+def app_noticias_resumos_template(request):
+    total = Pessoa.objects.count()
+    return render(request, 'app_noticias/resumo.html', {'total_pessoas': total})
+
+def noticia_detalhes(request, noticia_id):
+    try: 
+        noticia = Noticia.objects.get(pk=noticia_id)
+    except Noticia.DoesNotExist:
+        raise Http404('Notícia não encontrada')
+    return render(request, 'app_noticias/detalhes.html', {'noticia': noticia})
